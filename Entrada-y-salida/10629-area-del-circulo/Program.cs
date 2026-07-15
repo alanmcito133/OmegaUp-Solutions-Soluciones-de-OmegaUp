add44592dfe0740152aa5ed80419b5a5cfe3c581
todoriki <alanmc0303@gmail.com>
2026-07-14 20:24:20 -0600

    feat: add C# implementation for area of circle

---
+++ Entrada-y-salida/10629-area-del-circulo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Numerics;
+
+public class Program
+{
+    public static void Main()
+    {
+        double radio = double.Parse(Console.ReadLine());
+        double area = Math.PI * (radio * radio);
+        Console.WriteLine($"{area:F2}");
+    }
+}
+
